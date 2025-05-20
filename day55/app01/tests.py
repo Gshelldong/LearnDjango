25bab016553757314d7b39f28df90525cd83a5e7
gongxiaoliao <gongxiaoliao@kailinjt.com>
2025-05-20 17:50:51 +0800

    修改test.py

--- day55/app01/tests.py
+++ day55/app01/tests.py
@@ -4,7 +4,7 @@ from django.test import TestCase
 
 # Create your tests here.
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'day54.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'day55.settings')
 
 import django
 django.setup()
