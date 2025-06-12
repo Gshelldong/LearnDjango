8e5195dee74ca37fb52abbbefaa9dc2a26d1f7be
gongxiaoliao <gongxiaoliao@kailinjt.com>
2025-06-12 17:58:22 +0800

    day58部分

--- day58/day58/urls.py
+++ day58/day58/urls.py
@@ -23,4 +23,9 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('reg/', views.reg, name='reg'),
     path('lg/', app02_views.lg, name='lg'),
+    path('home/', views.home, name='home'),
+    path('loginout/', views.loginout, name='loginout'),
+    path('set_session/', views.set_session, name='set_session'),
+    path('get_session/', views.get_session, name='get_session'),
+    path('detail/', views.detail, name='detail'),
 ]
